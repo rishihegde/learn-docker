da22c3c59e14bf1e533b2966556d5a836c211838
Rishi <rishi@rishi-pc>
2022-06-02 10:38:05 +0530

    update test

--- test-build.sh
+++ test-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -o errexit
-op=$(docker run --rm hello-docker)
+op=$(docker run --rm learn-docker/hello-docker:1.0.0)
 if [[ "$op" == "Hello world" ]]
 then
 	echo 'Test Passed. Output expected'
